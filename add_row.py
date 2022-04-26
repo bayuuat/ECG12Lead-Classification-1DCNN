3d47c963df0424a450d69925bf61be6668a17829
bayuuat <bayuuat@gmail.com>
2022-04-26 23:49:02 +0700

    done: add row

---
+++ add_row.py
@@ -0,0 +1,17 @@
+from csv import writer
+from csv import reader
+
+default_text = 'N'
+# Open the input_file in read mode and output_file in write mode
+with open('data.csv', 'r') as read_obj, open('output_1.csv', 'w', newline='') as write_obj:
+    # Create a csv.reader object from the input file object
+    csv_reader = reader(read_obj)
+    # Create a csv.writer object from the output file object
+    csv_writer = writer(write_obj)
+    # Read each row of the input csv file as list
+    for row in csv_reader:
+        # Append the default text in the row / list
+        row.append(default_text)
+        print(row)
+        # Add the updated row / list to the output file
+        csv_writer.writerow(row)
